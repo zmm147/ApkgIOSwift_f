5764003227776aa5ab50a591f4e885004034fdcb
zmm147 <109190493+zmm147@users.noreply.github.com>
2025-04-06 10:23:15 +0800

    Update Package.swift
    .package(url: "https://github.com/readium/ZIPFoundation.git", from: "3.0.0"),

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/weichsel/ZIPFoundation.git", .upToNextMajor(from: "0.9.0")),
+        .package(url: "https://github.com/readium/ZIPFoundation.git", from: "3.0.0"),
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.15.3"),
         .package(url: "https://github.com/facebook/zstd.git", from: "1.5.0"),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.0.0")

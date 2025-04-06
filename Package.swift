d85692a7381fe9285bcc4f8093c90d6c3c008285
zmm147 <109190493+zmm147@users.noreply.github.com>
2025-04-06 11:50:46 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/readium/ZIPFoundation.git", from: "3.0.0"),
+        .package(url: "https://github.com/readium/ZIPFoundation.git", .upToNextMajor(from: "3.0.0")),
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.15.3"),
         .package(url: "https://github.com/facebook/zstd.git", from: "1.5.0"),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.0.0")
@@ -22,7 +22,7 @@ let package = Package(
         .target(
             name: "ApkgIO",
             dependencies: [
-                "ZIPFoundation",
+                .product(name: "ZIPFoundation", package: "ZIPFoundation"), // 明确指定
                 .product(name: "SQLite", package: "sqlite.swift"),
                 .product(name: "libzstd", package: "zstd"),
                 .product(name: "SwiftProtobuf", package: "swift-protobuf")
